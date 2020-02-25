194531b5dd07b5ef31f3115a2496a84ccf3d384f
Haydn Bannister <haydn.j.bannister@gmail.com>
2020-02-25 20:55:12 +1300

    stop edges and corner intersections from triggering block landing

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -17,13 +17,34 @@ public class Block : MonoBehaviour
     {
         
     }
+
+    public Vector3 GetAbsoluteCoordinates()
+    {
+        return transform.position;
+    }
     
     private void OnTriggerEnter(Collider other)
     {
+
         if (transform.parent == other.transform.parent)
             return;
-        
+
         print("shape " + gameObject.name + " has collided with " + other.gameObject.name);
+        
+        if (other.transform.parent.name == "GameGrid")
+        {
+            GetComponentInParent<Shape>().inPlay = false;
+            return;
+        }
+        
+        // only hit blocks directly below, otherwise corners and edges count
+        var thisPosition = transform.position;
+        var otherPosition = other.transform.position;
+        if (thisPosition.x != otherPosition.x || thisPosition.z != otherPosition.z)
+        {
+            return;
+        }
+        
         GetComponentInParent<Shape>().inPlay = false;
     }
 }

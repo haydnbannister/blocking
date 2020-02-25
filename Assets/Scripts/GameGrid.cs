7aa9eaa9ab1e08d20da85d68608d1f9c41832a0d
Haydn Bannister <haydn.j.bannister@gmail.com>
2020-02-25 18:54:34 +1300

    add rudimentary horizontal controls for shapes

--- Assets/Scripts/GameGrid.cs
+++ Assets/Scripts/GameGrid.cs
@@ -16,6 +16,6 @@ public class GameGrid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            transform.Translate(Vector3.forward * 1);
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            transform.Translate(Vector3.back * 1);
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            transform.Translate(Vector3.left * 1);
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            transform.Translate(Vector3.right * 1);
+        }
+    }
+}

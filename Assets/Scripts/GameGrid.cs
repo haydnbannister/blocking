d9e5a9c6c0173844854070e4eaca3deb3fa86692
Haydn Bannister <haydn.j.bannister@gmail.com>
2020-02-25 18:36:52 +1300

    add skeleton scripts for basic classes

---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The three dimensional grid that cubes are stored in
+public class GameGrid : MonoBehaviour
+{
+	GameObject[,,] cubes = new GameObject[5, 5, 25];
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

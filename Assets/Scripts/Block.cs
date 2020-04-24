baebb0d8faaa6a0be0b7215ef41269521ad440c6
Haydn Bannister <haydn.j.bannister@gmail.com>
2020-04-24 10:31:47 +0100

    Round block positions to int before collision detection comparison

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -30,7 +30,8 @@ public class Block : MonoBehaviour
         // only hit blocks directly below, otherwise corners and edges count
         var thisPosition = transform.position;
         var otherPosition = other.transform.position;
-        if (thisPosition.x != otherPosition.x || thisPosition.z != otherPosition.z)
+        if ((int) Math.Round(thisPosition.x, 0) != (int) Math.Round(otherPosition.x, 0) 
+            || (int) Math.Round(thisPosition.z, 0) != (int) Math.Round(otherPosition.z, 0) )
         {
             return;
         }
